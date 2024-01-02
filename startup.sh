901c1927992c4916e4bc678665e6da39dc289823
ITZ ARCTIX <144344981+teamarctix@users.noreply.github.com>
2024-01-02 21:52:21 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,8 +1,8 @@
-sudo apt-get update -y && sudo apt-get upgrade -y
+apt-get update -y && apt-get upgrade -y
 wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_linux
 mv yt-dlp_linux yt-dlp
 chmod 777 yt-dlp
-sudo apt install ffmpeg -y
-sudo apt install aria2 -y
+apt install ffmpeg -y
+apt install aria2 -y
 pip install pyrogram && pip install tgcrypto && pip install vcsi && pip install moviepy
 echo "Package Downloaded and Set"
