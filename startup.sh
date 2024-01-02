d9ba1138204627dd592a156bf8460e71d87db80e
ITZ ARCTIX <144344981+teamarctix@users.noreply.github.com>
2024-01-02 23:38:54 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,8 +2,4 @@ apt-get update -y && apt-get upgrade -y
 wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_linux
 mv yt-dlp_linux yt-dlp
 chmod 777 yt-dlp
-apt install ffmpeg -y
-apt install aria2 -y
-pip install pyrogram && pip install tgcrypto && pip install vcsi && pip install moviepy
-echo "Package Downloaded and Set"
 python3 sibu.py
