b1a83165004d291b29d4643fa66a860c56b5af2b
ITZ ARCTIX <144344981+teamarctix@users.noreply.github.com>
2024-01-03 22:34:55 +0530

    Update sibu.py

--- sibu.py
+++ sibu.py
@@ -1,15 +1,14 @@
 import os
 from threading import Thread
+os.system("pip install pyrofork==2.2.11")
 from pyrogram import Client, filters
 import random
 import time
 
-#os.system("wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O yt-dlp \
-    #&& chmod +x yt-dlp")
-#print("Current working directory:", os.getcwd())
+
 api_id = 11405252
 api_hash = "b1a1fc3dc52ccc91781f33522255a880"
-bot_token2 = "6326333011:AAHHvjzDx7zc8nKXzobh_dNRoS5yH7KTPmw"
+bot_token2 = "6593397412:AAFmJ8Hj9jnZuvLs_rLcu63bQwCp0EV829w"
 
 app = Client("my_bot",api_id=api_id, api_hash=api_hash,bot_token=bot_token2)
 
@@ -19,19 +18,28 @@ up = { "ytdl": False,'Total':0}
 
 
 links=[
-    "https://www.pornhub.com/playlist/85569291"
+    "https://youtube.com/playlist?list=PLMC9KNkIncKvYin_USF1qoJQnIyMAfRxl&si=w7qSDmBdsGWslf4u"
+    #"https://www.pornhub.com/playlist/85569291"
       ]
 
 
 
-async def progress(current, total):
-    print(f"{current * 100 / total:.1f}%")
+#async def progress(current, total):
+    #print(f"{current * 100 / total:.1f}%")
 
+async def progress(current, total, task_name="Task"):
+    percentage = current * 100 / total
+    bar_length = 20
+    completed_blocks = int(bar_length * current / total)
+
+    bar = "█" * completed_blocks + " " * (bar_length - completed_blocks)
+
+    print(f"{task_name} Progress: [{bar}] {percentage:.1f}% Complete")
 
 
 def ytdlpp(link):
      #time.sleep(2)
-    os.system("""./yt-dlp --downloader aria2c --match-filter "duration>180"   --max-downloads  200  -N 10 -o '%(title)s.%(ext)s' -f '(mp4)[height=?720]' --write-thumbnail --embed-metadata """ + link )
+     os.system("""./yt-dlp --downloader aria2c --match-filter "duration>180"   --max-downloads  200  -N 10 -o '%(title)s.%(ext)s' -f '(mp4)[height=?720]' --write-thumbnail --embed-metadata """ + link )
      print(f"Download Completed{link}")
      time.sleep(120)
      up["ytdl"] = True
@@ -65,8 +73,7 @@ async def main():
                                 pass
                         except Exception as e:
                             print(e)
-                            if up["ytdl"] == True:
-                             upl = False
-                   
+                    if up["ytdl"] == True:
+                        upl = False
 print("Bot Started")
 app.run(main())
