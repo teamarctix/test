17aa66f15149e2d15ac969484eb83ce7fdc6e86e
ITZ ARCTIX <144344981+teamarctix@users.noreply.github.com>
2024-03-10 15:14:58 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from pymongo import MongoClient
+
+# Replace the placeholder with your MongoDB connection string
+mongo_uri = 'mongodb+srv://mongo:mongo@mongo.1ics4jt.mongodb.net/?retryWrites=true&w=majority'
+
+client = MongoClient(mongo_uri)
+
+# Replace the placeholders with your database and collection names
+database_name = 'wzmlx'
+collection_name = 'data'
+
+# Access the specified database and collection
+db = client[database_name]
+collection = db[collection_name]
+
+# Query MongoDB and convert the result to a DataFrame
+cursor = collection.find()
+df = pd.DataFrame(list(cursor))
+
+# Display the DataFrame
+print(df)
