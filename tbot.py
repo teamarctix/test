548bf2e30ad560a286cbd53437d0bfd6cf9d99c1
ITZ ARCTIX <144344981+teamarctix@users.noreply.github.com>
2024-03-11 20:00:36 +0530

    Update tbot.py

--- tbot.py
+++ tbot.py
@@ -5,7 +5,7 @@ import os
 bot_token = '6126230406:AAFAtz4AhVLbSEnm7KabLzDa7d5Yf0_Mo2I'
 api_id = '11405252'
 api_hash = 'b1a1fc3dc52ccc91781f33522255a880'
-target_user_id = '-1001986522281'  # Replace with the actual user ID to whom you want to send the message
+target_user_id = '1881720028'  # Replace with the actual user ID to whom you want to send the message
 
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
